299cfdaa6ca858fb3f824f399812eb82a1c2dead
Marcos Paulo <itsolutions031250@gmail.com>
2022-11-28 17:58:35 -0300

    Add project files.

---
+++ Pessoas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeirosExercicios01_02
+{
+    internal class Pessoas
+    {
+        public string Name;
+        public int Age;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using primeirosExercicios01_02;
+using System.Security.Cryptography;
+
+Pessoas p1, p2;
+
+p1 = new Pessoas();
+p2 = new Pessoas();
+
+
+Console.WriteLine("Dados da primeira pessoa:");
+Console.Write("Nome: ");
+p1.Name = Console.ReadLine();
+Console.Write("Idade: ");
+p1.Age = int.Parse(Console.ReadLine());
+Console.WriteLine("Dados da segunda pessoa:");
+Console.Write("Nome: ");
+p2.Name = Console.ReadLine();
+Console.Write("Idade: ");
+p2.Age= int.Parse(Console.ReadLine());
+
+if(p1.Age > p2.Age)
+{
+    Console.WriteLine("Pessoa mais velha: " + p1.Name);
+}
+else
+{
+    Console.WriteLine("Pessoa mais velha: " + p2.Name);
+}
